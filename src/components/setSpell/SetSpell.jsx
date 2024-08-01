77dbce985579026148ad0ea2837c4891d983adb2
Dridou <nc.adrien@hotmail.com>
2024-08-02 01:14:14 +0200

    Set spell stable

--- src/components/setSpell/SetSpell.jsx
+++ src/components/setSpell/SetSpell.jsx
@@ -1,17 +1,40 @@
-﻿import React from 'react';
+﻿import React from "react";
 import Image from "next/image";
-import styles from "./setSpell.module.css";
+import styles from "./SetSpell.module.css";
 
-const SetSpell = ({ standardImage, opponentImage, opponentSpells, explanation, timings, alternatives }) => {
+const SetSpell = ({
+  standardImage,
+  opponentImage,
+  opponentSpells,
+  explanation,
+  timings,
+  alternatives,
+}) => {
   return (
-    <div>
-      <h4 className={styles.setDivTitle}>Spells</h4>
-      <div className={styles.setImageContainer}>
-        <Image src={standardImage} className={styles.setImage} alt="Spells and timing" width={20} height={20}  />
+    <div className={styles.container}>
+      <h4 className={styles.title}>Spells</h4>
+      <div className={styles.mainSpells}>
+        <div className={styles.imageContainer}>
+        	<Image
+	          src={standardImage}
+	          className={styles.setImage}
+	          alt="Spells and timing"
+	          width={609}
+	          height={154}
+	        />
+        </div>
       </div>
       <div className={styles.setDetails}>
-        <h5>Oponnent spells ({opponentSpells}):</h5>
-        <Image src={opponentImage} className={styles.setImageSmall} alt="Opponent spells" width={20} height={20} />
+        <h5 className={styles.subTitle}>Oponnent spells ({opponentSpells}):</h5>
+          <div className={styles.imageContainer}>
+          	<Image
+	            src={opponentImage}
+	            className={styles.setImageSmall}
+	            alt="Opponent spells"
+	            width={609}
+	            height={109}
+	          />
+          </div>
         <h5>Brief explanation:</h5>
         <p>{explanation}</p>
         <h5>Timings:</h5>
