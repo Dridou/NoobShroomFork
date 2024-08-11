55cdfa3c59cda86e2bfc15a357ba7c93c9999f99
Dridou <nc.adrien@hotmail.com>
2024-08-11 16:46:46 +0200

    Fix quill client side

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -13,8 +13,11 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
+import dynamic from 'next/dynamic';
 
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 const WritePage = () => {
   const { status } = useSession();
   const router = useRouter();
