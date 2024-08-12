f24ce3eb0c30cc6d3b1cf8cba63bd7e52d0b48ac
Dridou <nc.adrien@hotmail.com>
2024-08-12 06:29:09 +0200

    cache config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com",'cdn.discordapp.com']
+	images: {
+	  domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", 'cdn.discordapp.com'],
+	},
+	async headers() {
+	  return [
+		{
+		  source: "/api/posts/:slug", // S'applique aux routes API sous /api/posts/
+		  headers: [
+			{
+			  key: "Cache-Control",
+			  value: "s-maxage=86400, stale-while-revalidate=59", // Cache pendant 24 heures
+			},
+		  ],
+		},
+		// Ajoute d'autres configurations de cache ici si nécessaire
+	  ];
+	},
   }
-}
 
-module.exports = nextConfig
+  module.exports = nextConfig;
