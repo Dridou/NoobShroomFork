36ce684da0e6bf4fd1907f7fe2b555812f2313bc
Dridou <nc.adrien@hotmail.com>
2024-08-12 03:40:34 +0200

    fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
