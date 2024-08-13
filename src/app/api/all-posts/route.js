0084578087aede0d69622fc239c54454662c2aac
Dridou <nc.adrien@hotmail.com>
2024-08-13 08:41:54 +0200

    Remove where cat slug for all-posts

--- src/app/api/all-posts/route.js
+++ src/app/api/all-posts/route.js
@@ -3,13 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const posts = await prisma.post.findMany({
-      where: {
-        catSlug: {
-          notIn: ["legal", "shops", "database"],
-        },
-      },
-    });
+    const posts = await prisma.post.findMany();
 
     return NextResponse(JSON.stringify({ posts }, { statue: 200 }));
   } catch (error) {
