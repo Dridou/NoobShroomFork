01bea49f194230f22a67b68940ecbe4580568c2d
Dridou <nc.adrien@hotmail.com>
2024-08-13 06:51:28 +0200

    Fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,19 @@
 ﻿module.exports = {
-	siteUrl: 'https://www.noobshroom.com/', // Remplacez par l'URL de votre site
-	generateRobotsTxt: true,  // Génère un fichier robots.txt
-	changefreq: 'daily',     // Fréquence de changement des pages
-	priority: 0.7,            // Priorité par défaut pour les URLs
-	sitemapSize: 7000,        // Nombre maximum d'URLs par fichier sitemap
+	siteUrl: 'https://www.noobshroom.com',
+	generateRobotsTxt: true, // (optional)
+	sitemapSize: 7000,
+	// Optionnel: définir une fonction pour récupérer les pages dynamiques
+	async additionalPaths(config) {
+	  const paths = [];
+
+	  // Par exemple, si vous avez des posts de blog :
+	  const res = await fetch(`$https://www.noobshroom.com/api/posts`);
+	  const posts = await res.json();
+
+	  posts.forEach((post) => {
+		paths.push({ loc: `/posts/${post.slug}`, changefreq: 'weekly', priority: 0.7 });
+	  });
+
+	  return paths;
+	},
   };
