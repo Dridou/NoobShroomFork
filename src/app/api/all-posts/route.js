c3491fda61279c3cd137436b584671562a49d3ae
Dridou <nc.adrien@hotmail.com>
2024-08-13 09:09:11 +0200

    fix

--- src/app/api/all-posts/route.js
+++ src/app/api/all-posts/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
   try {
-	const posts = await prisma.$transaction([prisma.post.findMany()]);
+	const posts = await prisma.post.findMany();
 
     return new NextResponse(JSON.stringify({ posts }, { status: 200 }));
   } catch (error) {
