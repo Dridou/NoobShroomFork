25f598de2899f4d379b8e5077ff49d564a148a51
Dridou <nc.adrien@hotmail.com>
2024-08-13 06:33:44 +0200

    SEO sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+	siteUrl: 'https://www.noobshroom.com/', // Remplacez par l'URL de votre site
+	generateRobotsTxt: true,  // Génère un fichier robots.txt
+	changefreq: 'daily',     // Fréquence de changement des pages
+	priority: 0.7,            // Priorité par défaut pour les URLs
+	sitemapSize: 7000,        // Nombre maximum d'URLs par fichier sitemap
+  };
