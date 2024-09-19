1438cc4e3b930951244714cecaed777e25c804b6
Dridou <nc.adrien@hotmail.com>
2024-09-20 01:46:02 +0200

    Modify font to poppins

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Poppins  } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import GoogleAnalytics from "@/components/GoogleAnalytics/GoogleAnalytics";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 import AuthProvider from "@/providers/AuthProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ['100','200','300','400','500','600','700'],subsets: ["latin"] });
 
 export const metadata = {
   title: "Legend of Mushroom - Wiki",
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <GoogleAnalytics />
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <AuthProvider>
           <ThemeContextProvider>
             <ThemeProvider>
