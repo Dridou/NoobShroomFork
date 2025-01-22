d324d41e2d294f23d99105784415e4f18eafc4b7
Dridou <nc.adrien@hotmail.com>
2025-01-22 18:54:50 +0100

    Authorize friend of slooth

--- src/components/EditSectionButton/EditSectionButton.jsx
+++ src/components/EditSectionButton/EditSectionButton.jsx
@@ -8,12 +8,11 @@ const EditSectionButton = ({ sectionId , postId}) => {
   const { data: session } = useSession();
   const [isEditing, setIsEditing] = useState(false);
 
-  const authorizedUsers = ["aneboncarle@hotmail.fr", "blacksnake1234451@gmail.com"];
+  const authorizedUsers = ["aneboncarle@hotmail.fr", "blacksnake1234451@gmail.com", "bieze.mike@gmail.com"];
   const partialAuthorizedUsers = ["joyboy06210@hotmail.fr"];
   const magePosts = ['cm0wn8t4a000acfyajmdmr0jp', 'clzjvah2y000j4z4auy3dxiw5'];
-  const isUserAuthorized =
-    session?.user?.email && authorizedUsers.includes(session.user.email);
-	const isPartialUserAuthorizedForSpecialPage = session?.user?.email && partialAuthorizedUsers.includes(session.user.email) && magePosts.includes(postId);
+  const isUserAuthorized = session?.user?.email && authorizedUsers.includes(session.user.email);
+  const isPartialUserAuthorizedForSpecialPage = session?.user?.email && partialAuthorizedUsers.includes(session.user.email) && magePosts.includes(postId);
   const toggleEdit = () => setIsEditing(!isEditing);
 
   if (!isUserAuthorized && !isPartialUserAuthorizedForSpecialPage) return null; // Ne rien afficher si l'utilisateur n'est pas autorisé
