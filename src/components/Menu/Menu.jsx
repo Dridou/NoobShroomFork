0c3832904f654613b02d395b4690834a92098a53
Dridou <nc.adrien@hotmail.com>
2025-12-21 23:15:31 +0100

    Drop unused MenuCategories import

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,7 +3,6 @@ import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import MenuPosts from "../menuPosts/MenuPosts";
-import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
